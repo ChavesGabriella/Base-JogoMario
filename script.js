9f081e4278279a4d8b8e751b562d5739969b6d2c
ChavesGabriella <gabriellasoeirooo@gmail.com>
2023-12-07 14:52:08 -0300

    função de pontuação e reiniciar jogo funcionando

--- script.js
+++ script.js
@@ -3,79 +3,65 @@ const cano = document.querySelector('.cano')
 const nuvem = document.querySelector('.nuvem')
 const fimDeJogo = document.querySelector('.fim-de-jogo')
 const botaoReiniciar = document.querySelector('.reiniciar')
+const ponto = document.querySelector('.ponto');
 
 let pontuacao = 0
+
+function atualizarPontuacao() {
+  /*console.log('Pontuação: ' + pontuacao)
+  const elementoPontuacao = document.querySelector('.pontuacao')
+  if (elementoPontuacao) {
+    elementoPontuacao.innerText = 'Pontuação: ' + pontuacao
+  }*/
+  ponto.innerHTML = "Pontuação: " + pontuacao;
+}
+
+let loopJogo = setInterval(verificarColisoes, 10);
+
+function pararJogo() {
+  clearInterval(loopJogo);
+  console.log("Jogo em pausa");
+}
 
 function fazerMarioPular() {
   mario.classList.add('pular')
   setTimeout(function () {
-    mario.classList.remove('pular')
-    const posicaoCano = cano.offsetLeft
-    const posicaoMario = parseFloat(getComputedStyle(mario).bottom)
-
-    if (posicaoCano <= 100 && posicaoCano > 0 && posicaoMario < 60) {
-      pontuacao++
-      atualizarPontuacao()
-    }
-
-    mario.style.bottom = '80px' 
-  }, 300)
-}
-
-document.addEventListener('keyup', fazerMarioPular)
-
-function atualizarPontuacao() {
-  console.log('Pontuação: ' + pontuacao)
-  const elementoPontuacao = document.querySelector('.pontuacao')
-  if (elementoPontuacao) {
-    elementoPontuacao.innerText = 'Pontuação: ' + pontuacao
-  }
+    mario.classList.remove('pular');
+    pontuacao++
+    atualizarPontuacao()
+  }, 500);
 }
 
 function verificarColisoes() {
   const posicaoCano = cano.offsetLeft
-  const posicaoMario = parseFloat(getComputedStyle(mario).bottom)
-  const posicaoNuvem = parseFloat(getComputedStyle(nuvem).left)
+  const posicaoMario = parseFloat(getComputedStyle(mario).bottom);
+  const posicaoNuvem = parseFloat(getComputedStyle(nuvem).bottom);
 
   if (posicaoCano <= 100 && posicaoCano > 0 && posicaoMario < 60) {
-    pontuacao++
-    atualizarPontuacao() 
     console.log('Colisão detectada! Pontuação atual: ' + pontuacao)
+    pontuacao = 0;
+    pararJogo();
+
+    cano.style.animation = 'none';
+    cano.style.left = `${posicaoCano}px`;
+  
+    mario.style.animation = 'none';
+    mario.style.bottom = `${posicaoCano}px`;
+    mario.src = 'assets/imgs/fim-de-jogo.png';
+    mario.style.width = '70px';
+    mario.style.marginLeft = '35px';
+  
+    nuvem.style.animation = 'nuvem 20s infinite linear';
+    nuvem.style.left = `${posicaoNuvem}px`;
+  
+    fimDeJogo.style.visibility = 'visible';
   }
-
-  pararJogo()
-
-  cano.style.animation = 'none'
-  cano.style.left = `${posicaoCano}px`
-
-  mario.style.animation = 'none'
-  mario.style.bottom = `${posicaoCano}px`
-  mario.src = 'assets/imgs/fim-de-jogo.png'
-  mario.style.width = '70px'
-  mario.style.marginLeft = '35px'
-
-  nuvem.style.animation = 'nuvem 20s infinite linear'
-  nuvem.style.left = `${posicaoNuvem}px`
-
-  fimDeJogo.style.visibility = 'visible'
-}
-
-function pararJogo() {
-  clearInterval(loopJogo)
-  console.log('Jogo Parado')
 }
 
 function reiniciarJogo() {
-    pontuacao = 0;
-    fimDeJogo.style.visibility = 'hidden';
-    mario.src = 'assets/imgs/mario.png';
-    mario.style.width = '60px';
-    mario.style.marginLeft = '0';
-    mario.style.bottom = '80px';
-    cano.style.animation = 'cano 2s infinite linear';
-    nuvem.style.animation = 'nuvem 20s infinite linear';
-
-    loopJogo = setInterval(verificarColisoes, 10);
-
-    atualizarPontuacao(); 
+  location.reload();
+  atualizarPontuacao();
 }
+
+document.addEventListener("keyup", fazerMarioPular);
+document.addEventListener("click", reiniciarJogo);
